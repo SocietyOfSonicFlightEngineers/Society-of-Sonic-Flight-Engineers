96a024565721749e5854ea71cfd6b6f46a42d7aa
DhruvTAMU <dhruvmulugu1@tamu.edu>
2026-02-22 17:11:48 -0600

    Navigation Menu Condensing Update
    Hamburger thing

--- script.js
+++ script.js
@@ -1,7 +1,24 @@
+console.log("JS loaded");
+
 document.querySelectorAll('h3').forEach(header => {
   header.addEventListener('click', () => {
     const dropdown = header.parentElement;
     const expanded = dropdown.classList.toggle('open');
     header.setAttribute('aria-expanded', expanded);
   });
+});
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    const hamburger = document.getElementById("hamburger");
+    const navmenu = document.getElementById("navmenu");
+
+    hamburger.addEventListener("click", function () {
+        navmenu.classList.toggle("active");
+                      if (navmenu.classList.contains("active")) {
+                hamburger.textContent = ">";
+            } else {
+                hamburger.textContent = "☰";
+            }
+    });
 });
