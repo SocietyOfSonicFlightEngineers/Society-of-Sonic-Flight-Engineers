44ad44bc764f023f2153e0cbac1964a630e5771d
BraveAviator <94481150+BraveAviator@users.noreply.github.com>
2026-02-25 21:29:59 -0600

    Major bug fixes
    Fixed the navbar by making it look more professor and cleaner. Allowed the projects dropdown to actually dropdown. fixed the image references that I have and changed things to make it look more professional.

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
+
 console.log("JS loaded");
 
 document.querySelectorAll('h3').forEach(header => {
